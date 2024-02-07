d2d895a9ebc96a1c10ae0f3876600cf2ec31b146
Zilola Ixtiyorova <118010424+Zilolagit@users.noreply.github.com>
2024-02-07 23:43:22 +0500

    first commit

---
+++ src/MovieCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const MovieCard = ({movie}) => {
+    return (
+        <div className="movie">
+            <div>
+                <p>{movie.Year}</p>
+            </div>
+
+            <div>
+                <img src={movie.Poster !== 'N/A' ? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
+            </div>
+
+            <div>
+                <span>
+                    {movie.Type}
+                    <h3>{movie.Title}</h3>
+                </span>
+            </div>
+
+        </div>
+    );
+}
+
+export default MovieCard;
